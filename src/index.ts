34166305c0b86d3b50129cf924cf68dbc112ec16
FlorenciaRusso9606 <russoflorencia96@gmail.com>
2026-01-13 11:38:44 -0300

    Fix: Update an env variable

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { app } from './app.js'
 const BACKEND_URL =
   process.env.NODE_ENV === "production"
     ? process.env.API_URL
-    : `http://localhost:${PORT}`;
+    : `http://localhost:${process.env.PORT}`;
 app.listen(3000, () => {
   console.log(`Servidor corriendo en ${BACKEND_URL}`)
 })
