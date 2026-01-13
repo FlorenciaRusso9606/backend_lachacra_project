edf4990bd8b245b95ef7d2a0901162c70f5f2f0a
FlorenciaRusso9606 <russoflorencia96@gmail.com>
2026-01-13 11:33:16 -0300

    Update URL server

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { app } from './app.js'
+const PORT =  process.env.NODE_ENV === "production" ? :  8080
 
+const BACKEND_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.API_URL
+    : `http://localhost:${PORT}`;
 app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000')
+  console.log(`Servidor corriendo en ${BACKEND_URL}`)
 })
